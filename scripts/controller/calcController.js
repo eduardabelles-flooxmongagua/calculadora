efa33b4c73d769b4a5f68238b7e5504a490c733f
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-02-25 19:49:01 -0300

    feat: add event click button

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -7,6 +7,7 @@ class CalcController{
   this._timeEl = document.querySelector("#hora");
   this._currentDate;
   this.initialize();
+  this.initButtonsEvents();
  
 }
 
@@ -18,9 +19,24 @@ initialize() {
   }, 1000);
 }
 
+    initButtonsEvents(){
+      let buttons = document.querySelectorAll("#buttons>g,#parts>g");
+
+      buttons.forEach((btn,index)=>{
+        btn.addEventListener('click', e =>{
+          console.log(btn.className.baseVal.replace("btn-",""));
+        });
+      })
+    }
+
+
 setDisplayDateTime() {
  
-  this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+  this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+    day: "2-digit",
+    month: "long",
+    year: "numeric"
+  });
   this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 }
 
