c58a5dfa4c53da99c760445771a372f106700799
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-02-26 07:59:03 -0300

    feat: add calc method to evaluate operations and improve operation handling

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -34,11 +34,71 @@ this._operation=[];
 clearEntry(){
 this._operation.pop();
 }
-addOperation(value){
+
+getLastOperation() {
+  return this._operation[this._operation.length - 1]; 
+}
+
+setLastOperation(value) {
+  this._operation[this._operation.length - 1] = value;
+}
+
+
+isOperation(value){
+return (['+', '-', '*','%' ,'/'].indexOf(value)>-1)
+ 
+
+}
+
+pushOperation(value){
 this._operation.push(value);
 
-console.log(this._operation);
+if(this._operation.length>3){
+
+  this.calc();
+  //console.log(this._operation);  
 }
+
+}
+
+calc(){
+  let last=this._operation.pop();
+  let result=eval(this._operation.join(""));
+  this._operation= [result,last];
+}
+
+setLastNumberToDisplay(){
+ 
+}
+
+addOperation(value) {
+  //console.log('A', value, isNaN(this.getLastOperation()));
+
+  if (isNaN(this.getLastOperation())) {
+    if (this.isOperation(value)) {
+
+      this.setLastOperation(value);
+    } else if (isNaN(value)) {
+
+      console.log('Outra coisa', value)
+    } else {
+      this.pushOperation(value);
+    }
+  } else {
+    if (this.isOperation(value)) {
+      this.pushOperation(value);
+    } else {
+      let newValue = this.getLastOperation().toString() + value.toString();
+      this.setLastOperation(parseInt(newValue));
+
+      this.setLastNumberToDisplay();
+    }
+  }
+
+  console.log(this._operation);
+}
+
+
 
 setError(){
   this.displayCalc="Error";
@@ -55,22 +115,33 @@ execBtn(value){
 
       break;
       case 'soma':
+      this.addOperation('+');
 
       break;
       case 'subtracao':
+      this.addOperation('-');
 
       break;
       case 'divisao':
+      this.addOperation('/');
 
       break;
       case 'multiplicacao':
+      this.addOperation('*');
     
       break;
       case 'porcento':
+      this.addOperation('%');
     
       break;
       case 'igual':
-  break;
+
+      break;
+      case 'ponto':
+      this.addOperation('.');
+     break;
+  
+
     case '0':
     case '1':
     case '2':
